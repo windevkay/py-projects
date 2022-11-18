ef730c3dcbe36857d1f0bf5ca87e0e563c41e3a9
Kola Adebayo <kolaadebayo@Kolas-MacBook-Pro.local>
2022-11-18 16:13:00 -0700

    initial commit

---
+++ odot/main.py
@@ -0,0 +1,21 @@
+user_prompt = "Type add ,show (or display) or exit: "
+todos = []
+
+while True:
+    user_action = input(user_prompt)
+    user_action = user_action.strip()
+
+    match user_action:
+        case 'add':
+            todo = input("Enter a todo: ")
+            todos.append(todo)
+        case 'show' | 'display':
+            for item in todos:
+                item = item.title()
+                print(item)
+        case 'exit':
+            break
+        case _:
+            print("Unknown or Invalid command")
+
+print("...exiting!")
