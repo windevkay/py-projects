59a20a7a4c1e5f509f4d7f23c273a3b8757cb85a
Kola Adebayo <kolaadebayo@Kolas-MacBook-Pro.local>
2022-11-28 14:55:41 -0700

    interpolating strings

--- odot/main.py
+++ odot/main.py
@@ -10,14 +10,18 @@ while True:
             todo = input("Enter a todo: ")
             todos.append(todo)
         case 'show' | 'display':
-            for item in todos:
+            for index, item in enumerate(todos):
                 item = item.title()
-                print(item)
+                print(f"{index + 1}- {item}")
         case 'edit':
             index = int(input("Index of todo to edit: "))
             new_todo = input("Enter new todo: ")
             # replace existing todo with the new one
             todos[index - 1] = new_todo
+        case 'complete':
+            index = int(input("Index of todo to complete: "))
+            # removes the todo from the list
+            todos.pop(index - 1)
         case 'exit':
             break
         case _:
