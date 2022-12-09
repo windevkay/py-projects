ad84e2746f8dc001b4c04936c0be52d631bb083f
Kola Adebayo <kolaadebayo@Kolas-MacBook-Pro.local>
2022-12-08 22:04:15 -0700

    reading/writing to disk

--- odot/main.py
+++ odot/main.py
@@ -1,27 +1,52 @@
-user_prompt = "Type add, show (or display), edit or exit: "
-todos = []
-
 while True:
-    user_action = input(user_prompt)
-    user_action = user_action.strip()
+    user_action = input("Type add, show (or display), edit, complete or exit: ").strip()
 
     match user_action:
         case 'add':
-            todo = input("Enter a todo: ")
+            todo = input("Enter a todo: ") + "\n"
+            # read existing todos
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
+
             todos.append(todo)
+            # save todo on disk
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
+
         case 'show' | 'display':
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
+            # remove trailing newline chars - list comp
+            # new_todos = [item.strip('\n') for item in todos]
             for index, item in enumerate(todos):
-                item = item.title()
+                item = item.strip('\n').title()
                 print(f"{index + 1}- {item}")
+
         case 'edit':
-            index = int(input("Index of todo to edit: "))
-            new_todo = input("Enter new todo: ")
+            index = int(input("Index of todo to edit: ")) - 1
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
+
+            new_todo = input("Enter new todo: ") + '\n'
             # replace existing todo with the new one
-            todos[index - 1] = new_todo
+            todos[index] = new_todo
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
+
         case 'complete':
-            index = int(input("Index of todo to complete: "))
+            index = int(input("Index of todo to complete: ")) - 1
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
+
+            todo_to_remove = todos[index].strip('\n')
             # removes the todo from the list
-            todos.pop(index - 1)
+            todos.pop(index)
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
+
+            message = f"Todo: {todo_to_remove} - was removed from the list"
+            print(message)
+
         case 'exit':
             break
         case _:
