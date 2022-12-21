c14951634d0332019c1200370355b23d6813e852
Kola Adebayo <kolaadebayo@Kolas-MBP.lan>
2022-12-21 16:01:06 -0700

    refactor

--- odot/main.py
+++ odot/main.py
@@ -1,53 +1,64 @@
+def get_todos():
+    with open('todos.txt', 'r') as fileReader:
+        result = fileReader.readlines()
+    return result
+
+
+def write_todos(args):
+    with open('todos.txt', 'w') as fileWriter:
+        fileWriter.writelines(args)
+
+
 while True:
     user_action = input("Type add, show, edit, complete or exit: ").strip()
     # match params and cases can be used as well
-    if 'add' in user_action:
+    if user_action.startswith('add'):
         # extract everything else that comes after 'add' (slicing)
         todo = user_action[4:] + "\n"
         # read existing todos
-        with open('todos.txt', 'r') as file:
-            todos = file.readlines()
+        todos = get_todos()
 
         todos.append(todo)
         # save item on disk
-        with open('todos.txt', 'w') as file:
-            file.writelines(todos)
+        write_todos(todos)
 
-    elif 'show' in user_action:
-        with open('todos.txt', 'r') as file:
-            todos = file.readlines()
+    elif user_action.startswith('show'):
+        todos = get_todos()
         # remove trailing newline chars - list comp
         # new_todos = [item.strip('\n') for item in todos]
         for index, item in enumerate(todos):
             item = item.strip('\n').title()
             print(f"{index + 1}- {item}")
 
-    elif 'edit' in user_action:
-        index = int(user_action[5:]) - 1
-        with open('todos.txt', 'r') as file:
-            todos = file.readlines()
+    elif user_action.startswith('edit'):
+        try:
+            index = int(user_action[5:]) - 1
+            todos = get_todos()
 
-        new_todo = input("Enter new todo: ") + '\n'
-        # replace existing item with the new one
-        todos[index] = new_todo
-        with open('todos.txt', 'w') as file:
-            file.writelines(todos)
+            new_todo = input("Enter new todo: ") + '\n'
+            # replace existing item with the new one
+            todos[index] = new_todo
+            write_todos(todos)
+        except ValueError:
+            print('Invalid Command')
+            continue
 
-    elif 'complete' in user_action:
-        index = int(user_action[9:]) - 1
-        with open('todos.txt', 'r') as file:
-            todos = file.readlines()
+    elif user_action.startswith('complete'):
+        try:
+            index = int(user_action[9:]) - 1
+            todos = get_todos()
 
-        todo_to_remove = todos[index].strip('\n')
-        # removes the item from the list
-        todos.pop(index)
-        with open('todos.txt', 'w') as file:
-            file.writelines(todos)
+            todo_to_remove = todos[index].strip('\n')
+            # removes the item from the list
+            todos.pop(index)
+            write_todos(todos)
+            message = f"Todo: {todo_to_remove} - was removed from the list"
+            print(message)
+        except IndexError:
+            print('No item with that index')
+            continue
 
-        message = f"Todo: {todo_to_remove} - was removed from the list"
-        print(message)
-
-    elif 'exit' in user_action:
+    elif user_action.startswith('exit'):
         break
     else:
         print('Invalid Command')
